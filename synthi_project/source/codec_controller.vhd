dd805e15b5ff0c2cf95556cc264816e78d05b48f
heinipas <120182015+heinipas@users.noreply.github.com>
2024-03-05 15:21:10 +0100

    synthi_top integration
    - changed coded_controller.vhd to template from moodle
    - synthi_top expanded with coded_controller and i2c_master
    - added codec_controller, i2c_master and reg_table_pkg.vhd to quartus

--- synthi_project/source/codec_controller.vhd
+++ synthi_project/source/codec_controller.vhd
@@ -1,61 +1,54 @@
--------------------------------------------------------------------------------
--- Title      : codec_controller
--- Project    : synthi
--------------------------------------------------------------------------------
--- File       : codec_controller.vhd
--- Author     : doblesam, grundale, heinipas
--- Company    : 
--- Created    : 2024-03-05
--- Last update: 2024-03-05
--- Platform   : 
--- Standard   : VHDL'08
--------------------------------------------------------------------------------
--- Description: block for codec controller
--------------------------------------------------------------------------------
--- Copyright (c) 2024 
--------------------------------------------------------------------------------
--- Revisions  :
--- Date        Version  Author  Description
--- 2024-03-05  1.0      heini   Created
--------------------------------------------------------------------------------
+--------------------------------------------------------------------
+--
+-- Project     : Audio_Synth
+--
+-- File Name   : codec_controller.vhd
+-- Description : Controller to define Audio Codec Configuration via I2C
+--                                      
+-- Features:    Der Baustein wartet bis das reset_n signal inaktiv wird.
+--              Danach sendet dieser Codec Konfigurierungsdaten an
+--              den Baustein i2c_Master
+--                              
+--------------------------------------------------------------------
+-- Change History
+-- Date       |Name      |Modification
+--------------|----------|------------------------------------------
+-- 2019-03-07 | gelk     | Prepared template for students
+-- 2024-03-05 | heinipas | beautify template
+--------------------------------------------------------------------
 
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+use work.reg_table_pkg.all;
 
--------------------------------------------------------------------------------
 
 entity codec_controller is
 
   port (
+    mode         : in  std_logic_vector(2 downto 0);  -- Inputs to choose Audio_MODE
+    write_done_i : in  std_logic;       -- Input from i2c register write_done
+    ack_error_i  : in  std_logic;       -- Inputs to check the transmission
     clk          : in  std_logic;
     reset_n      : in  std_logic;
-    mode         : in  std_logic_vector(2 downto 0);
-    write_done_i : in  std_logic;
-    ack_error_i  : in  std_logic;
-    write_o      : out std_logic;
-    write_data_o : out std_logic_vector(15 downto 0)
+    write_o      : out std_logic;       -- Output to i2c to start transmission 
+    write_data_o : out std_logic_vector(15 downto 0)  -- Data_Output
     );
+end codec_controller;
 
-end entity codec_controller;
 
--------------------------------------------------------------------------------
+-- Architecture Declaration
+-------------------------------------------
+architecture rtl of codec_controller is
+-- Signals & Constants Declaration
+-------------------------------------------
 
-architecture str of codec_controller is
+-- Begin Architecture
+-------------------------------------------
+begin
 
-  -----------------------------------------------------------------------------
-  -- Internal signal declarations
-  -----------------------------------------------------------------------------
 
-  -----------------------------------------------------------------------------
-  -- Component declarations
-  -----------------------------------------------------------------------------
-
-begin  -- architecture str
-
-  -----------------------------------------------------------------------------
-  -- Component instantiations
-  -----------------------------------------------------------------------------
-
-end architecture str;
-
--------------------------------------------------------------------------------
+-- End Architecture 
+------------------------------------------- 
+end rtl;
