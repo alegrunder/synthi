85a0ab0683e172b06e762251f4f5565fa9b589c4
DeTintefisch <35544630+DeTintefisch@users.noreply.github.com>
2024-03-12 11:33:22 +0100

    updated Testbench (#22)

--- synthi_project/source/synthi_top_tb.vhd
+++ synthi_project/source/synthi_top_tb.vhd
@@ -130,18 +130,18 @@ architecture struct of synthi_top_tb is
   signal gpi_signals : std_logic_vector(31 downto 0);
   signal I2C_SDAT  : std_logic := 'H';
   signal I2C_SCLK  : std_logic := 'H';
-  signal reg_data0 : std_logic_vector(31 downto 0);
-  signal reg_data1 : std_logic_vector(31 downto 0);
-  signal reg_data2 : std_logic_vector(31 downto 0);
-  signal reg_data3 : std_logic_vector(31 downto 0);
-  signal reg_data4 : std_logic_vector(31 downto 0);
-  signal reg_data5 : std_logic_vector(31 downto 0);
-  signal reg_data6 : std_logic_vector(31 downto 0);
-  signal reg_data7 : std_logic_vector(31 downto 0);
-  signal reg_data8 : std_logic_vector(31 downto 0);
-  signal reg_data9 : std_logic_vector(31 downto 0);
+  signal reg_data_0 : std_logic_vector(31 downto 0);
+  signal reg_data_1 : std_logic_vector(31 downto 0);
+  signal reg_data_2 : std_logic_vector(31 downto 0);
+  signal reg_data_3 : std_logic_vector(31 downto 0);
+  signal reg_data_4 : std_logic_vector(31 downto 0);
+  signal reg_data_5 : std_logic_vector(31 downto 0);
+  signal reg_data_6 : std_logic_vector(31 downto 0);
+  signal reg_data_7 : std_logic_vector(31 downto 0);
+  signal reg_data_8 : std_logic_vector(31 downto 0);
+  signal reg_data_9 : std_logic_vector(31 downto 0);
 begin  -- architecture struct
-
+SW(9 downto 0) <= gpi_signals(9 downto 0);
   -- component instantiation
   DUT: synthi_top
     port map (
@@ -175,7 +175,24 @@ begin  -- architecture struct
       LEDR_8      => LEDR_8,
       LEDR_9      => LEDR_9);
 
-  
+    -- instance "i2c_slave_bfm_1"
+    i2c_slave_bfm_1: i2c_slave_bfm
+    generic map (
+      verbose => verbose)
+    port map (
+      AUD_XCK   => AUD_XCK,
+      I2C_SDAT  => I2C_SDAT,
+      I2C_SCLK  => I2C_SCLK,
+      reg_data0 => reg_data_0,
+      reg_data1 => reg_data_1,
+      reg_data2 => reg_data_2,
+      reg_data3 => reg_data_3,
+      reg_data4 => reg_data_4,
+      reg_data5 => reg_data_5,
+      reg_data6 => reg_data_6,
+      reg_data7 => reg_data_7,
+      reg_data8 => reg_data_8,
+      reg_data9 => reg_data_9);
 
   readcmd : process
     -- This process loops through a file and reads one line
@@ -239,29 +256,54 @@ begin  -- architecture struct
 
        -- add further test commands below here
       elsif cmd.all = "uart_send_data" then
-	uar_sim(tv, usb_txd);
+	      uar_sim(tv, usb_txd);
 
       elsif cmd.all = "check_display_hex0" then 
-	hex_chk(tv, hex0); 
+	      hex_chk(tv, hex0); 
 	  
       elsif cmd.all = "check_display_hex1" then 
         hex_chk(tv, hex1);
         
       elsif cmd.all = "set_switches" then
-        gpi_sim(tv, gpi_signals);        
+        gpi_sim(tv, gpi_signals); 
+               
         
       elsif cmd.all = "check_i2c_reg_0" then
-        gpo_chk(tv,reg_data0);
+        gpo_chk(tv,reg_data_0);
 
       elsif cmd.all = "check_i2c_reg_1" then
-        gpo_chk(tv,reg_data1);
+        gpo_chk(tv,reg_data_1);
+      
+      elsif cmd.all = "check_i2c_reg_2" then
+        gpo_chk(tv,reg_data_2);
+    
+      elsif cmd.all = "check_i2c_reg_3" then
+        gpo_chk(tv,reg_data_3);
+
+      elsif cmd.all = "check_i2c_reg_4" then
+        gpo_chk(tv,reg_data_4);
+
+      elsif cmd.all = "check_i2c_reg_5" then
+        gpo_chk(tv,reg_data_5);
+
+      elsif cmd.all = "check_i2c_reg_6" then
+        gpo_chk(tv,reg_data_6);
+
+      elsif cmd.all = "check_i2c_reg_7" then
+        gpo_chk(tv,reg_data_7);
+
+      elsif cmd.all = "check_i2c_reg_8" then
+        gpo_chk(tv,reg_data_8);
+
+      elsif cmd.all = "check_i2c_reg_9" then
+        gpo_chk(tv,reg_data_9);
 
       else
         assert false
           report "NO MATCHING COMMAND FOUND IN 'testcase.dat' AT LINE: "& integer'image(lincnt)
           severity error;
       end if;
-      SW(9 downto 0) <= gpi_signals(9 downto 0);
+      
       
       if tv.fail_flag = true then --count failures in tests
         fail_counter := fail_counter + 1;
@@ -283,24 +325,7 @@ begin  -- architecture struct
 
   end process clkgen;
 
-  -- instance "i2c_slave_bfm_1"
-  i2c_slave_bfm_1: i2c_slave_bfm
-    generic map (
-      verbose => verbose)
-    port map (
-      AUD_XCK   => AUD_XCK,
-      I2C_SDAT  => I2C_SDAT,
-      I2C_SCLK  => I2C_SCLK,
-      reg_data0 => reg_data0,
-      reg_data1 => reg_data1,
-      reg_data2 => reg_data2,
-      reg_data3 => reg_data3,
-      reg_data4 => reg_data4,
-      reg_data5 => reg_data5,
-      reg_data6 => reg_data6,
-      reg_data7 => reg_data7,
-      reg_data8 => reg_data8,
-      reg_data9 => reg_data9);
+
 
   
 
